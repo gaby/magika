4f875af290c935f229d37f14d0deb325313efff4
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2024-02-24 19:31:39 -0500

    Run verbose test

--- python/tests/utils_magika_python_client.py
+++ python/tests/utils_magika_python_client.py
@@ -41,7 +41,9 @@ def run_magika_python_cli(
     model_dir: Optional[Path] = None,
     extra_cli_options: Optional[List[str]] = None,
 ) -> Tuple[str, str]:
-    cmd = ["python", "-m", "magika"]
+    cmd = [
+        "magika",
+    ]
     cmd.extend(map(str, samples_paths))
     if model_dir is not None:
         cmd.append(str(model_dir))
